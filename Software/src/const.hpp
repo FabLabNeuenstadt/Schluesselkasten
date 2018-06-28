d6382916bd9be568b6d959dc126e53cc24bc3389
GityUpNow <kim-cheng@gmx.de>
2018-06-28 19:03:36 +0200

    Added a file for all the constants

---
+++ Software/src/const.hpp
@@ -0,0 +1,22 @@
+#ifndef CONST_HPP
+#define CONST_HPP
+
+#define LATCH_PIN 0
+#define CLOCK_PIN 2
+#define DATA_PIN 5
+#define CS 16
+//TODO: Remove the PowerPin from the PCB
+#define POWER_PIN 4
+#define REED_PIN 5
+#define PIN_UNUSED 100
+
+#define BUFFER_SIZE 8192
+
+#define BLINK_DELAY 250
+
+#define NUM_KEYS 64
+#define PLUG_KEYS 9
+#define BLOCK_LENGTH 16
+//TODO:Add the File-Lenght here
+
+#endif
