b6587e17c61897159704dfb4fde57302dc661efe
GityUpNow <kim-cheng@gmx.de>
2018-06-28 18:24:47 +0200

    Purged old Code

--- Software/power.cpp
+++
@@ -1,9 +0,0 @@
-#include "power.hpp"
-#include <Arduino.h>
-
-bool powerState = false;
-
-void setPowerState(bool state){
-  powerState = state;
-  digitalWrite(powerPin, !powerState);
-}
--- Software/power.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef POWER_HPP
-#define POWER_HPP
-
-#define powerPin 4
-extern bool powerState;
-void setPowerState(bool state);
-
-#endif
